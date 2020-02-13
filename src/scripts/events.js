d7f4086f62fcc787d3ead21bb76c669bb4e28094
castle <castlesmadeofcode@gmail.com>
2020-02-13 12:44:24 -0600

    edit feature is working, need to figure out how to target only review and cost without minupulating other data

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -5,9 +5,41 @@ const interestName = document.getElementById("interestName");
 const interestDescription = document.getElementById("interestDescription");
 const interestCost = document.getElementById("interestCost");
 const interestReview = document.getElementById("interestReview")
+const interestLocation = document.getElementById("interestLocation")
+
 const saveButton = document.getElementById("saveButton");
 const interestsContainer = document.getElementById("interestsContainer");
+const hiddenInterestId = document.querySelector("#interestId")
 
+
+const clearForm = () => {
+  
+    hiddenInterestId.value = "";
+    interestName.value = "";
+    interestDescription.value = "";
+    interestCost.value = "";
+    interestReview.value = "";
+    interestLocation.value = "";
+  }
+
+
+const updateInterestFields = interestId => {
+
+    fetch(`http://localhost:5000/interests/${interestId}`)
+        .then(response => response.json())
+        .then(interest => {
+            console.log(interest)
+
+            hiddenInterestId.value = interest.id;
+            interestCost.value = interest.cost;
+            interestReview.value = interest.review;
+            interestName.value = interest.name;
+            interestDescription.value = interest.description;
+            // interestLocation.value = interest.location.name;
+        
+
+        })
+}
 
 
 const events = {
@@ -21,16 +53,35 @@ const events = {
                 "review": interestReview.value,
                 "placeId": interestLocation.value
             }
+            if (hiddenInterestId.value !== "") {
+                newInterest.id = parseInt(hiddenInterestId.value);
             console.log(newInterest);
+
+
+            API.updateInterestEntry(newInterest)
+                .then(render.renderAllInterests)
+                .then(clearForm);
+
+            }
+            else if (interestName.value !== "" && interestDescription.value !== ""
+            && interestCost.value !== "" && interestReview.value !== "" && interestLocation.valye !== "") {
+
+            interestsContainer.textContent = ""
 
             API.addNewInterest(newInterest)
                 .then(render.renderAllInterests)
+                .then(clearForm);
+
+        }
+        else {
+            window.alert("fill out all the things!")
+        }
         })
     },
     addDeleteButtonListeners() {
         interestsContainer.addEventListener("click", (event) => {
 
-            if (event.target.id.startsWith("delete--")) {
+            if (event.target.id.startsWith("deleteInterest--")) {
                 const deleteBtnId = event.target.id.split("--")[1];
 
                 interestsContainer.textContent = ""
@@ -38,10 +89,17 @@ const events = {
                 API.deleteInterest(deleteBtnId)
                     .then(render.renderAllInterests)
             }
+            else if (event.target.id.startsWith("editInterest--")) {
+                const interestToEdit = event.target.id.split("--")[1];
+
+                updateInterestFields(interestToEdit)
+
+            }
 
         })
+
+        }
     }
-}
 
 
 export default events
